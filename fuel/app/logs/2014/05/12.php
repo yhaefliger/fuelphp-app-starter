cf475b8bede84b2b53dbc3508e75e5e1a0352f27
Yann Haefliger <yhaefliger@gmail.com>
2014-05-12 13:58:03 +0200

    base fuelphp 1.7.1 extract

---
+++ fuel/app/logs/2014/05/12.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2014-05-12 11:57:16 --> Error - date_default_timezone_get(): It is not safe to rely on the system's timezone settings. You are *required* to use the date.timezone setting or the date_default_timezone_set() function. In case you used any of those methods and you are still getting this warning, you most likely misspelled the timezone identifier. We selected 'Europe/Paris' for '2.0/DST' instead in C:\UwAmp\www\OTHER\fstart\fuel\core\classes\fuel.php on line 161
